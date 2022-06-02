3876c7feff2e679f55ed929e553deafef11459ef
jinsu4755 <parkjinsu4755@gmail.com>
2022-06-02 18:59:27 +0900

    feat(seminar3-lv2): Create Response Wrapper
    - 받아온 Data나 status,message 를 통해 ResponseWarpDTO 를 만들기 위한 래퍼 클래스 작성

---
+++ Seminar3/src/utils/ResponseWrapper.ts
@@ -0,0 +1,22 @@
+import ResponseWrapDTO from "../interfaces/common/ResponsWrapDTO";
+
+
+export default class ResponseWrapper {
+    static successOf<T>(status: number, messgae: string, data?: T): ResponseWrapDTO<T> {
+        return {
+            status: status,
+            message: messgae,
+            success: true,
+            data: data
+        }
+    }
+
+    static failureOf<T>(status: number, messgae: string, data?: T): ResponseWrapDTO<T> {
+        return {
+            status: status,
+            message: messgae,
+            success: false,
+            data: data
+        }
+    }
+};
